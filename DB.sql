3165ab5321bad3f62494f1fc45e4e21264c3da02
minh-3016 <74865161+minh3016@users.noreply.github.com>
2024-09-04 15:18:48 +0700

    Add files via upload

---
+++ DB.sql
@@ -0,0 +1,114 @@
+Updatecheck<|||>3<|||>1
+DoUsageStatistics<|||>3<|||>1
+UpdatecheckLastrun<|||>1<|||>2024-09-03 23:34:05
+LastUsageStatisticCall<|||>1<|||>2024-09-03 23:34:10
+LastSessions<|||>1<|||>BIKE STORE MANAGEMENT
+LastActiveSession<|||>1<|||>BIKE STORE MANAGEMENT
+SessionManager_ListWidth<|||>3<|||>260
+SessionManager_WindowHeight<|||>3<|||>474
+SessionManager_WindowLeft<|||>3<|||>543
+SessionManager_WindowTop<|||>3<|||>133
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>4,1
+MainWinOnMonitor<|||>3<|||>0
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,428
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,428,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,300
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,368
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+CreateDbCollation<|||>1<|||>utf8mb4_general_ci
+ColWidths_frmTableEditor.listColumns<|||>1<|||>32,100,152,90,60,65,50,117,130,139,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>191,100,80,120
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>76,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,296
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+querymemoheight<|||>3<|||>599
+queryhelperswidth<|||>3<|||>99
+dbtreewidth<|||>3<|||>214
+MainWinLeft<|||>3<|||>952
+MainWinTop<|||>3<|||>0
+MainWinWidth<|||>3<|||>781
+MainWinHeight<|||>3<|||>822
+MainWinMaximized<|||>3<|||>1
+ExportSQL_Filenames<|||>1<|||>DataQLVN<{{{><}}}>
+ExportSQL_CreateDatabases<|||>3<|||>1
+ExportSQL_CreateTables<|||>3<|||>1
+DateTimeEditor_CursorPos_Type4<|||>3<|||>0
+sqloutheight<|||>3<|||>131
+SQLFile0<|||>1<|||>M:\Prf_C_Basic\Bike_Store_Management\DB.sql
+SQLFile1<|||>1<|||>C:\Users\PC\AppData\Roaming\HeidiSQL\Backups\query-tab-2023-07-31_17-00-57-720.sql
+Servers\BIKE STORE MANAGEMENT\SessionCreated<|||>1<|||>2024-09-03 23:34:25
+Servers\BIKE STORE MANAGEMENT\Host<|||>1<|||>127.0.0.1
+Servers\BIKE STORE MANAGEMENT\WindowsAuth<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\CleartextPluginEnabled<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\User<|||>1<|||>root
+Servers\BIKE STORE MANAGEMENT\Password<|||>1<|||>1
+Servers\BIKE STORE MANAGEMENT\LoginPrompt<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\Port<|||>1<|||>3306
+Servers\BIKE STORE MANAGEMENT\NetType<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\Compressed<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\LocalTimeZone<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\QueryTimeout<|||>3<|||>30
+Servers\BIKE STORE MANAGEMENT\KeepAlive<|||>3<|||>20
+Servers\BIKE STORE MANAGEMENT\FullTableStatus<|||>3<|||>1
+Servers\BIKE STORE MANAGEMENT\Databases<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\Library<|||>1<|||>libmariadb.dll
+Servers\BIKE STORE MANAGEMENT\Comment<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\StartupScriptFilename<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\TreeBackground<|||>3<|||>536870911
+Servers\BIKE STORE MANAGEMENT\SSHtunnelActive<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\SshExecutable<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\SSHtunnelHost<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\SSHtunnelHostPort<|||>3<|||>22
+Servers\BIKE STORE MANAGEMENT\SSHtunnelUser<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\SSHtunnelPassword<|||>1<|||>2
+Servers\BIKE STORE MANAGEMENT\SSHtunnelTimeout<|||>3<|||>4
+Servers\BIKE STORE MANAGEMENT\SSHtunnelPrivateKey<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\SSHtunnelPort<|||>3<|||>3307
+Servers\BIKE STORE MANAGEMENT\SSL_Active<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\SSL_Key<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\SSL_Cert<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\SSL_CA<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\SSL_Cipher<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\IgnoreDatabasePattern<|||>1<|||>
+Servers\BIKE STORE MANAGEMENT\LogFileDdl<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\LogFileDml<|||>3<|||>0
+Servers\BIKE STORE MANAGEMENT\LogFilePath<|||>1<|||>C:\Users\PC\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\BIKE STORE MANAGEMENT\RefusedCount<|||>3<|||>4
+Servers\BIKE STORE MANAGEMENT\ServerVersionFull<|||>1<|||>10.4.28-MariaDB - mariadb.org binary distribution
+Servers\BIKE STORE MANAGEMENT\ConnectCount<|||>3<|||>2
+Servers\BIKE STORE MANAGEMENT\ServerVersion<|||>3<|||>100428
+Servers\BIKE STORE MANAGEMENT\LastConnect<|||>1<|||>2024-09-04 15:02:38
+Servers\BIKE STORE MANAGEMENT\lastUsedDB<|||>1<|||>bike store management
